89a8b2ae591df3964fb1449dd51b7cda49a18dd4
Daniel Lamb <daniellamb113@gmail.com>
2019-09-21 14:56:46 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,4 +1,12 @@
-
+contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        binding.pry
+      end
+    end
+  end
+end
   
 def remove_strawberry(contacts)
  contacts.each do |person, contact_details_hash|
